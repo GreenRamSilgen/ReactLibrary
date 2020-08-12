4f31267df8306a3219862fc4392a971df34fcefb
GreenRamSilgen <ps3iswow@gmail.com>
2020-08-12 14:30:20 -0700

    Updates

---
+++ precache-manifest.fd077c3930b79a80884938b516b49c81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d43d4e4fbda5beabd659ac8070b02ce5",
+    "url": "/ReactLibrary/index.html"
+  },
+  {
+    "revision": "b78da24df6006e13a088",
+    "url": "/ReactLibrary/static/css/main.b2528434.chunk.css"
+  },
+  {
+    "revision": "2e75ba47cf9d56f71976",
+    "url": "/ReactLibrary/static/js/2.2b2243e4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactLibrary/static/js/2.2b2243e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b78da24df6006e13a088",
+    "url": "/ReactLibrary/static/js/main.7d87ce51.chunk.js"
+  },
+  {
+    "revision": "9c9b3d1aa2c9b099ece7",
+    "url": "/ReactLibrary/static/js/runtime-main.033ad19a.js"
+  }
+]);
